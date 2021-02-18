a06fc558170cc7436b97e0c209316d72f972c4df
Leonardo <leonardo@fastinfo.com.br>
2021-02-18 20:46:51 -0300

    teste main

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-// JavaScript Document
+//pega o ID formulario sendo "chamado" pelo botão com a classe Submit
+document.getElementById('formulario').addEventListener('submit', cadastrarVeiculo);
+
+
+//função de cadastro
+function cadastrarVeiculo(e){
+	var modeloVeiculo = document.getElementById('modeloVeiculo').value;
+	var placaVeiculo = document.getElementById('placaVeiculo').value;
+	var time = new date();
+	
+	carro = {
+		modelo: modeloVeiculo,
+		placa: placaVeiculo,
+		hora: time.getHours(),
+		minutos: time.getMinutes(),
+		dia: time.getDay();
+	}
+	console.log(carro);
+	e.preventDefault();
+}
